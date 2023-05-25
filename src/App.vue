091ab0a7ee07294345356c5385e08b3dcbf5989c
carrotvegeta <1305115562@qq.com>
2023-05-25 10:50:27 +0800

    修改样式

--- src/App.vue
+++ src/App.vue
@@ -17,16 +17,20 @@ export default {
 </script>
 
 <style>
+html, body {
+    margin: 0;
+    padding: 0;
+}
+
 #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
-    width: 100vw;
+    width: 100%;
     color: #2c3e50;
     margin-top: 3em;
-    padding-left: 20em;
-    padding-right: 60em;
+    padding: 0 10%;
     box-sizing: border-box;
 }
 </style>
